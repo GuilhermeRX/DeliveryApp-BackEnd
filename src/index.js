a7b5a31e0b3928522ee1173fa7fab20fbd43bac3
Luiz Guilherme <guilhermerx18@gmail.com>
2022-06-28 19:37:55 -0300

    feat: add routes on index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 require('dotenv').config();
+const adressRouter = require('./routes/adress.route');
+const productsRouter = require('./routes/products.route');
+const requestsRouter = require('./routes/requests.route');
+const usersRouter = require('./routes/users.route');
 
 const app = express();
+app.use(express.json());
 const port = process.env.MYAPP_PORT;
+
+app.use('/adress', adressRouter);
+app.use('/products', productsRouter);
+app.use('/requests', requestsRouter);
+app.use('/users', usersRouter);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 

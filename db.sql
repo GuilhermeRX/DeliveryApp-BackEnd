0a173386555a0ecb1e4fa3c1e73d60d7f1374093
Luiz Guilherme <guilhermerx18@gmail.com>
2022-06-28 16:32:33 -0300

    feat: create table request_status, add FG on requests

--- db.sql
+++ db.sql
@@ -30,13 +30,20 @@ CREATE TABLE IF NOT EXISTS deliveryApp.users (
   updateAt DATETIME NULL ON UPDATE NOW()
 );
 
+CREATE TABLE IF NOT EXISTS deliveryApp.request_status (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS deliveryApp.requests (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   product_id INT NOT NULL,
   user_id INT NOT NULL,
   adress_id INT NOT NULL,
+  status_id INT NOT NULL,
   FOREIGN KEY (product_id) REFERENCES deliveryApp.products (id),
   FOREIGN KEY (user_id) REFERENCES deliveryApp.users (id),
   FOREIGN KEY (adress_id) REFERENCES deliveryApp.adress (id),
+  FOREIGN KEY (status_id) REFERENCES deliveryApp.request_status (id),
   createdAt DATETIME NOT NULL DEFAULT NOW()
 );

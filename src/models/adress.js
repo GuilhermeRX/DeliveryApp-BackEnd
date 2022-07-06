d6994db52e8b39038bbc0a5a734aa2b2b0fb1a1d
Luiz Guilherme <guilhermerx18@gmail.com>
2022-07-06 17:50:16 -0300

    feat: edit model adress

--- src/models/adress.js
+++ src/models/adress.js
@@ -1,23 +1,12 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class adress extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  adress.init({
-    cep: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'adress',
-  });
-  return adress;
-};
+const Adress = (sequelize, DataTypes) => {
+  const Adress = sequelize.define('adress', {
+    cep: DataTypes.STRING,
+    city: DataTypes.STRING,
+    district: DataTypes.STRING,
+    road: DataTypes.STRING,
+    number: DataTypes.INTEGER,
+  })
+  return Adress;
+}
+
+module.exports = Adress;

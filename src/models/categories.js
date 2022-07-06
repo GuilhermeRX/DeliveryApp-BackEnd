b9eb47958f18938784a23b481a440ec259779200
Luiz Guilherme <guilhermerx18@gmail.com>
2022-07-06 19:18:29 -0300

    feat: edit categories models

--- src/models/categories.js
+++ src/models/categories.js
@@ -1,23 +1,11 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class categories extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  categories.init({
-    name: DataTypes.STRING
+const Categories = (sequelize, DataTypes) => {
+  const Categories = sequelize.define('categories', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: DataTypes.STRING,
   }, {
-    sequelize,
-    modelName: 'categories',
-  });
-  return categories;
-};
+    underscored: true,
+  })
+  return Categories;
+}
+
+module.exports = Categories;

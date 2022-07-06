adc409ffbf6adac997854f4feec102ab9a1feec4
Luiz Guilherme <guilhermerx18@gmail.com>
2022-07-06 19:15:48 -0300

    feat: edit models users

--- src/models/adress.js
+++ src/models/adress.js
@@ -1,10 +1,17 @@
 const Adress = (sequelize, DataTypes) => {
   const Adress = sequelize.define('adress', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     cep: DataTypes.STRING,
     city: DataTypes.STRING,
     district: DataTypes.STRING,
     road: DataTypes.STRING,
     number: DataTypes.INTEGER,
+  }, {
+    underscored: true,
   })
   return Adress;
 }
--- src/models/users.js
+++ src/models/users.js
@@ -1,23 +1,26 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class users extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  users.init({
-    fullname: DataTypes.STRING
+const Users = (sequelize, DataTypes) => {
+  const Users = sequelize.define('users', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    fullname: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+    adress_id: DataTypes
   }, {
-    sequelize,
-    modelName: 'users',
-  });
-  return users;
-};
+    underscored: true,
+  })
+
+  Users.associate = (models) => {
+    Users.hasMany(models.adress,
+      { foreignKey: 'adress_id', as: 'adress' })
+  }
+
+
+  return Users;
+}
+
+module.exports = Users;

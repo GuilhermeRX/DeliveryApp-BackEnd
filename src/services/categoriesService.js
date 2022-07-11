b101c6df39c1ef330b298e9b35b4d3d4fd8e25fc
Luiz Guilherme <guilhermerx18@gmail.com>
2022-07-11 15:55:08 -0300

    feat: implement validateParamsId and Body on categories service

--- src/services/categoriesService.js
+++ src/services/categoriesService.js
@@ -1,6 +1,16 @@
+const Joi = require('joi');
 const { Category, Product } = require('../database/models');
+const runSchema = require('../schema/validate');
 
 const categoriesService = {
+  validateParamsId: runSchema(Joi.object({
+    id: Joi.number().required().integer().positive(),
+  })),
+
+  validateBody: runSchema(Joi.object({
+    name: Joi.string().required(),
+  })),
+
   getAll: async (includeProducts) => {
     if (includeProducts) {
       const categories = await Category.findAll(

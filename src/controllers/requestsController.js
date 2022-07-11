e7d1142c8128a86f9689a2a3a5af678a588074de
Luiz Guilherme <guilhermerx18@gmail.com>
2022-07-11 18:35:30 -0300

    feat: add request services and controllers

--- src/controllers/requestsController.js
+++ src/controllers/requestsController.js
@@ -1,3 +1,36 @@
-const requestsController = {};
+const requestsService = require('../services/requestsService');
+
+const requestsController = {
+  getAll: async (req, res) => {
+    const requests = await requestsService.getAll();
+    res.status(200).json(requests);
+  },
+
+  getById: async (req, res) => {
+    const { id } = requestsService.validateParamsId(req.params);
+    await requestsService.checkIfExists(id);
+    const request = await requestsService.getById(id);
+    res.status(200).json(request);
+  },
+
+  create: async (req, res) => {
+    const request = await requestsService.create(req.body);
+    res.status(201).json(request);
+  },
+
+  update: async (req, res) => {
+    const { id } = requestsService.validateParamsId(req.params);
+    await requestsService.checkIfExists(id);
+    await requestsService.update(req.body);
+    res.status(200).json({ id, ...req.body });
+  },
+
+  delete: async (req, res) => {
+    const { id } = requestsService.validateParamsId(req.params);
+    await requestsService.checkIfExists(id);
+    await requestsService.delete(id);
+    res.sendStatus(204);
+  },
+};
 
 module.exports = requestsController;
--- src/services/requestsService.js
+++ src/services/requestsService.js
@@ -1,3 +1,40 @@
-const requestsService = {};
+const Joi = require('joi');
+const runSchema = require('../schema/validate');
+const { Request } = require('../database/models');
+
+const requestsService = {
+  validateParamsId: runSchema(Joi.object({
+    id: Joi.number().required().integer().positive(),
+  })),
+
+  checkIfExists: async (id) => {
+    const request = await Request.findByPk(id);
+    if (!request) throw new Error('Request not exists');
+    return true;
+  },
+
+  getAll: async () => {
+    const requests = await Request.findAll();
+    return requests;
+  },
+
+  getById: async (id) => {
+    const request = await Request.findByPk(id);
+    return request;
+  },
+
+  create: async (object) => {
+    const request = await Request.create({ ...object });
+    return request;
+  },
+
+  update: async (object, id) => {
+    await Request.update({ ...object }, { where: { id } });
+  },
+
+  delete: async (id) => {
+    await Request.destroy({ where: { id } });
+  },
+};
 
 module.exports = requestsService;

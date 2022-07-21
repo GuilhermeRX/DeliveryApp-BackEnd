3a4fe557d491bc55e2067bee90ec5fcdbbbcc1cb
Luiz Guilherme <guilhermerx18@gmail.com>
2022-07-21 18:33:14 -0300

    fix

--- src/services/requestsService.js
+++ src/services/requestsService.js
@@ -78,7 +78,7 @@ const requestsService = {
             through: { attributes: [] },
             attributes: [],
           },
-          { model: User, as: 'user', attributes: ['fullname', 'email'] },
+          { model: User, as: 'user', attributes: ['id', 'fullname', 'email'] },
           { model: RequestStatus, as: 'status', attributes: ['name'] },
         ],
       attributes: ['id', [sequelize.fn('sum', sequelize.literal('value * quantity')), 'total']],
